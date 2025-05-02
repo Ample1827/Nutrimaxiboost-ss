4b7a48ce6ce87852359a20df6c70230135231672
Ample1827 <Adalromero99@gmail.com>
2025-05-01 21:59:36 -0600

    new path

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const topSellingProducts = [
     id: "1",
     name: "Premium Headphones",
     price: 2499.99,
-    image: "/images/whey.jpeg",
+    image: "/public/images/whey.jpeg",
     category: "featured",
     rating: 4.8,
     sales: 1245,
