c2b6245639d848e074abd9131cdee2661bc31d84
Ample1827 <Adalromero99@gmail.com>
2025-05-02 15:57:11 -0600

    new params

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,15 @@
-import { getProductById } from "@/lib/products"
+// app/product/[id]/page.tsx
+import { getProductById, getAllProducts } from "@/lib/products"
 import { notFound } from "next/navigation"
 import StoreLayout from "@/components/store-layout"
 import ProductDetail from "@/components/product-detail"
+
+export async function generateStaticParams() {
+  const products = getAllProducts() // Should return an array of all products
+  return products.map((product) => ({
+    id: product.id,
+  }))
+}
 
 export default function ProductDetailPage({ params }: { params: { id: string } }) {
   const product = getProductById(params.id)
